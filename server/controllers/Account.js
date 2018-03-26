14d5e034485cb75fec2521ed69de1432c9e1eb82
Irvin Do <irvindo@yahoo.com>
2018-03-26 05:05:45 -0400

    Ported to React

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -2,12 +2,19 @@ const models = require('../models');
 
 const Account = models.Account;
 
-const loginPage = (req, res) => {
-  res.render('login', { csrfToken: req.csrfToken() });
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
+const loginPage = (req, res) => {
+  res.render('login', { csrfToken: req.csrfToken() });
 };
 
 const logout = (req, res) => {
@@ -85,5 +92,5 @@ const signup = (request, response) => {
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
