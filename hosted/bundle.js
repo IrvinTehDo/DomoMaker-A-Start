eb72d73035bfa604344cd536ab358b8d106f05cd
Irvin Do <irvindo@yahoo.com>
2018-03-26 05:29:57 -0400

    mroe js fixes

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,135 +1,137 @@
+"use strict";
 
+var handleDomo = function handleDomo(e) {
+    e.preventDefault();
 
-const handleDomo = function handleDomo(e) {
-  e.preventDefault();
+    $("#domoMessage").animate({ width: 'hide' }, 350);
 
-  $('#domoMessage').animate({ width: 'hide' }, 350);
+    if ($("#domoName").val() == '' || $("#domoAge").val() == '') {
+        handleError("RAWR! All fields are required");
+        return false;
+    }
 
-  if ($('#domoName').val() == '' || $('#domoAge').val() == '') {
-    handleError('RAWR! All fields are required');
+    sendAjax('POST', $("#domoForm").attr("action"), $("#domoForm").serialize(), function () {
+        loadDomosFromServer();
+    });
+
     return false;
-  }
-
-  sendAjax('POST', $('#domoForm').attr('action'), $('#domoForm').serialize(), () => {
-    loadDomosFromServer();
-  });
-
-  return false;
 };
 
-const DomoForm = function DomoForm(props) {
-  return React.createElement(
-        'form',
-    { id: 'domoForm',
-      onSubmit: handleDomo,
-      name: 'domoForm',
-      action: '/maker',
-      method: 'POST',
-      className: 'domoForm',
-    },
+var DomoForm = function DomoForm(props) {
+    return React.createElement(
+        "form",
+        { id: "domoForm",
+            onSubmit: handleDomo,
+            name: "domoForm",
+            action: "/maker",
+            method: "POST",
+            className: "domoForm"
+        },
         React.createElement(
-            'label',
-            { htmlFor: 'name' },
-            'Name: '
+            "label",
+            { htmlFor: "name" },
+            "Name: "
         ),
-        React.createElement('input', { id: 'domoName', type: 'text', name: 'name', placeholder: 'Domo Name' }),
+        React.createElement("input", { id: "domoName", type: "text", name: "name", placeholder: "Domo Name" }),
         React.createElement(
-            'label',
-            { htmlFor: 'age' },
-            'Age: '
+            "label",
+            { htmlFor: "age" },
+            "Age: "
         ),
-        React.createElement('input', { id: 'domoAge', type: 'text', name: 'age', placeholder: 'Domo Age' }),
-        React.createElement('input', { type: 'hidden', name: '_csrf', value: props.csrf }),
-        React.createElement('input', { classNam: 'makeDomoSubmit', type: 'submit', value: 'Make Domo' })
+        React.createElement("input", { id: "domoAge", type: "text", name: "age", placeholder: "Domo Age" }),
+        React.createElement("input", { type: "hidden", name: "_csrf", value: props.csrf }),
+        React.createElement("input", { classNam: "makeDomoSubmit", type: "submit", value: "Make Domo" })
     );
 };
 
-const DomoList = function DomoList(props) {
-  if (props.domos.length === 0) {
-    return React.createElement(
-            'div',
-            { className: 'domoList' },
+var DomoList = function DomoList(props) {
+    if (props.domos.length === 0) {
+        return React.createElement(
+            "div",
+            { className: "domoList" },
             React.createElement(
-                'h3',
-                { className: 'emptyDomo' },
-                'No Domos yet'
+                "h3",
+                { className: "emptyDomo" },
+                "No Domos yet"
             )
         );
-  }
+    }
 
-  const domoNodes = props.domo.map((domo) => React.createElement(
-            'div',
-            { key: domo._id, className: 'domo' },
-            React.createElement('img', { src: '/assets/img/domoface.jpeg', alt: 'domo face', className: 'domoFace' }),
+    var domoNodes = props.domo.map(function (domo) {
+        return React.createElement(
+            "div",
+            { key: domo._id, className: "domo" },
+            React.createElement("img", { src: "/assets/img/domoface.jpeg", alt: "domo face", className: "domoFace" }),
             React.createElement(
-                'h3',
-                { className: 'domoName' },
-                ' Name: ',
+                "h3",
+                { className: "domoName" },
+                " Name: ",
                 domo.name,
-                ' '
+                " "
             ),
             React.createElement(
-                'h3',
-                { className: 'domoAge' },
-                ' Age: ',
+                "h3",
+                { className: "domoAge" },
+                " Age: ",
                 domo.age,
-                ' '
+                " "
             )
-        ));
+        );
+    });
 
-  return React.createElement(
-        'div',
-        { className: 'domoList' },
+    return React.createElement(
+        "div",
+        { className: "domoList" },
         domoNodes
     );
 };
 
 var loadDomosFromServer = function loadDomosFromServer() {
-  sendAjax('GET', '/getDomos', null, (data) => {
-    ReactDOM.render(React.createElement(DomoList, { domos: data.domos }), document.querySelector('#domos'));
-  });
+    sendAjax('GET', '/getDomos', null, function (data) {
+        ReactDOM.render(React.createElement(DomoList, { domos: data.domos }), document.querySelector("#domos"));
+    });
 };
 
-const setup = function setup(csrf) {
-  ReactDOM.render(React.createElement(DomoForm, { csrf }), document.querySelector('#makeDomo'));
+var setup = function setup(csrf) {
+    ReactDOM.render(React.createElement(DomoForm, { csrf: csrf }), document.querySelector("#makeDomo"));
 
-  ReactDOM.render(React.createElement(DomoForm, { domos: [] }), document.querySelector('#domos'));
+    ReactDOM.render(React.createElement(DomoForm, { domos: [] }), document.querySelector("#domos"));
 
-  loadDomosFromServer();
+    loadDomosFromServer();
 };
 
-const getToken = function getToken() {
-  sendAjax('GET', '/getToken', null, (result) => {
-    setup(result.csrfToken);
-  });
+var getToken = function getToken() {
+    sendAjax('GET', '/getToken', null, function (result) {
+        setup(result.csrfToken);
+    });
 };
 
-$(document).ready(() => {
-  getToken();
+$(document).ready(function () {
+    getToken();
 });
-'use strict';
+"use strict";
 
 var handleError = function handleError(message) {
-  $('#errorMessage').text(message);
-  $('domoMessage').animate({ width: 'toggle' }, 350);
+    $("#errorMessage").text(message);
+    $("#domoMessage").animate({ width: 'toggle' }, 350);
 };
 
-const redirect = function redirect(response) {
-  $('#domoMessage').animate({ width: 'hide' }, 350);
-  window.location = response.redirect;
+var redirect = function redirect(response) {
+    $("#domoMessage").animate({ width: 'hide' }, 350);
+    window.location = response.redirect;
 };
 
 var sendAjax = function sendAjax(type, action, data, success) {
-  $.ajax({
-    cache: false,
-    type,
-    url: action,
-    data,
-    dataType: 'json',
-    success,
-    error: function error(xhr, status, _error) {
-      const messageObj = JSON.parse(xhr.responseText);
-      handleError(messageObj.error);
-    },
-  });
+    $.ajax({
+        cache: false,
+        type: type,
+        url: action,
+        data: data,
+        dataType: "json",
+        success: success,
+        error: function error(xhr, status, _error) {
+            var messageObj = JSON.parse(xhr.responseText);
+            handleError(messageObj.error);
+        }
+    });
 };
--- hosted/loginBundle.js
+++ hosted/loginBundle.js
@@ -3,7 +3,7 @@
 var handleLogin = function handleLogin(e) {
   e.preventDefault();
 
-  $('domoMessage').animate({ width: 'hide' }, 350);
+  $('#domoMessage').animate({ width: 'hide' }, 350);
 
   if ($('#user').val() == '' || $('#pass').val() == '') {
     handleError('RAWR! Username or password is empty');
@@ -12,7 +12,7 @@ var handleLogin = function handleLogin(e) {
 
   console.log($('input[name=_csrf]').val());
 
-  sendAjax('POST', $('loginForm').attr('action'), $('#loginForm').serialize(), redirect);
+  sendAjax('POST', $('#loginForm').attr('action'), $('#loginForm').serialize(), redirect);
 
   return false;
 };
@@ -137,7 +137,7 @@ $(document).ready(function () {
 
 var handleError = function handleError(message) {
     $("#errorMessage").text(message);
-    $("domoMessage").animate({ width: 'toggle' }, 350);
+    $("#domoMessage").animate({ width: 'toggle' }, 350);
 };
 
 var redirect = function redirect(response) {
